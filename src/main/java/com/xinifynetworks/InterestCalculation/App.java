7f76a379ad178d0e35c812b9eff4d34fe42c54a6
Mahendra <Mahendra@Tharshanan.att.net>
2016-10-01 17:45:18 -0700

    Added functional interface for interest calculation

--- src/main/java/com/xinifynetworks/InterestCalculation/App.java
+++ src/main/java/com/xinifynetworks/InterestCalculation/App.java
@@ -2,25 +2,36 @@ package com.xinifynetworks.InterestCalculation;
 
 import java.util.Locale;
 
+import com.xinifynetworks.InterestCalculation.services.ISimpleInterest;
 import com.xinifynetworks.InterestCalculation.services.impl.SimpleInterest;
 
 public class App {
 	public static void main(String[] args) {
 		double principal = 1000;
 		float annualRate = 5;
+		int years = 7;
 
-		SimpleInterest annualSimpleInterest = new SimpleInterest();
+		//Functional Interface illustration
+		ISimpleInterest customInterest = (p, r, n) -> p * r * n / 100;
+		SimpleInterest forFormatting = new SimpleInterest();
+		
+		double ci = customInterest.calculateSimpleInterest(principal, annualRate, years);
+		System.out.println("Custom Interest (Unformatted)" + ci);
+		System.out.println("Custom Interest (Formatted to defrault locale)" + forFormatting.formattedCurrency(ci));
+		System.out.println("Custom Interest (Formatted to custom locale)" + forFormatting.formattedCurrency(ci, Locale.JAPAN));
 
-		System.out.println("Annual Simple Interest (Unformatted): "
-				+ annualSimpleInterest.yearlySimpleInterest(principal, annualRate));
+		SimpleInterest simpleInterest = new SimpleInterest();
+
+		System.out.println("\nAnnual Simple Interest (Unformatted): "
+				+ simpleInterest.yearlySimpleInterest(principal, annualRate));
 		System.out.println("Monthly Simple Interest (Unformatted): "
-				+ annualSimpleInterest.monthlySimpleInterest(principal, annualRate));
+				+ simpleInterest.monthlySimpleInterest(principal, annualRate));
 
-		System.out.println("\nFormatted Annual Simple Interest: " + annualSimpleInterest
-				.formattedCurrency(annualSimpleInterest.yearlySimpleInterest(principal, annualRate)));
-		System.out.println("Formatted Monthly Simple Interest: " + annualSimpleInterest
-				.formattedCurrency(annualSimpleInterest.monthlySimpleInterest(principal, annualRate), Locale.UK));
-		System.out.println("Formatted Daily Simple Interest: " + annualSimpleInterest
-				.formattedCurrency(annualSimpleInterest.perDiemSimpleInterest(principal, annualRate), Locale.CHINA));
+		System.out.println("\nFormatted Annual Simple Interest: "
+				+ simpleInterest.formattedCurrency(simpleInterest.yearlySimpleInterest(principal, annualRate)));
+		System.out.println("Formatted Monthly Simple Interest: "
+				+ simpleInterest.formattedCurrency(simpleInterest.monthlySimpleInterest(principal, annualRate), Locale.UK));
+		System.out.println("Formatted Daily Simple Interest: " + simpleInterest
+				.formattedCurrency(simpleInterest.perDiemSimpleInterest(principal, annualRate), Locale.CHINA));
 	}
 }
--- src/main/java/com/xinifynetworks/InterestCalculation/services/impl/SimpleInterest.java
+++ src/main/java/com/xinifynetworks/InterestCalculation/services/impl/SimpleInterest.java
@@ -3,9 +3,10 @@ package com.xinifynetworks.InterestCalculation.services.impl;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+import com.xinifynetworks.InterestCalculation.services.IPresentation;
 import com.xinifynetworks.InterestCalculation.services.ISimpleInterest;
 
-public class SimpleInterest implements ISimpleInterest {
+public class SimpleInterest implements IPresentation, ISimpleInterest {
 
 	public String formattedCurrency(double amount) {
 		return this.formattedCurrency(amount, Locale.getDefault());
@@ -15,6 +16,11 @@ public class SimpleInterest implements ISimpleInterest {
 		NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
 
 		return currencyFormatter.format(amount);
+	}
+
+	@Override
+	public double genericSimpleInterestCalculation(double principal, float annualRate, int years) {
+		return principal*annualRate*years/100;
 	}
 
 }
